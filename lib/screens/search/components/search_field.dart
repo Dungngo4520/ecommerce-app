e99bf193eaea6eacbbe66d576022439aa63db184
Dũng Ngo <dungngo4520@gmail.com>
2021-05-15 00:47:04 +0700

    add algolia search

--- lib/screens/search/components/search_field.dart
+++ lib/screens/search/components/search_field.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce/constants.dart';
+import 'package:ecommerce/models/Product.dart';
 import 'package:ecommerce/screens/search/components/search_input_provider.dart';
+import 'package:ecommerce/services/algolia_search.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +13,7 @@ class SearchField extends StatefulWidget {
 
 class _SearchFieldState extends State<SearchField> {
   TextEditingController searchInputController = TextEditingController();
+  FocusNode focusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     final seachInputState = Provider.of<SearchInputProvider>(context);
@@ -19,23 +22,28 @@ class _SearchFieldState extends State<SearchField> {
       decoration: BoxDecoration(
           color: cSecondaryColor.withOpacity(0.1),
           borderRadius: BorderRadius.circular(15)),
-      child: TextField(
-        controller: searchInputController,
-        onEditingComplete: () {
-          seachInputState.searchInput = searchInputController.text;
-        },
-        onTap: () {
-          searchInputController.selection = TextSelection(
-              baseOffset: 0, extentOffset: searchInputController.text.length);
-        },
-        decoration: InputDecoration(
-          enabledBorder: InputBorder.none,
-          focusedBorder: InputBorder.none,
-          hintText: 'Search products',
-          prefixIcon: Icon(Icons.search),
-          contentPadding: EdgeInsets.symmetric(
-              horizontal: getProportionateScreenWidth(20),
-              vertical: getProportionateScreenHeight(10)),
+      child: Focus(
+        child: TextField(
+          focusNode: focusNode,
+          autofocus: true,
+          controller: searchInputController,
+          onEditingComplete: () async {
+            seachInputState.searchInput = searchInputController.text;
+            focusNode.unfocus();
+          },
+          onTap: () {
+            searchInputController.selection = TextSelection(
+                baseOffset: 0, extentOffset: searchInputController.text.length);
+          },
+          decoration: InputDecoration(
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: 'Search products',
+            prefixIcon: Icon(Icons.search),
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20),
+                vertical: getProportionateScreenHeight(10)),
+          ),
         ),
       ),
     );
--- lib/screens/search/components/search_items.dart
+++ lib/screens/search/components/search_items.dart
@@ -1,8 +1,11 @@
+import 'package:algolia/algolia.dart';
+import 'package:ecommerce/components/loading_screen.dart';
 import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/models/Cart.dart';
 import 'package:ecommerce/models/Product.dart';
 import 'package:ecommerce/screens/details/details_screen.dart';
 import 'package:ecommerce/screens/search/components/search_input_provider.dart';
+import 'package:ecommerce/services/algolia_search.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -14,110 +17,128 @@ class SearchItems extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Cart> cartList = Provider.of<List<Cart>>(context);
     final seachInputState = Provider.of<SearchInputProvider>(context);
-    return seachInputState.searchInput == ""
-        ? Expanded(
-            child: Center(
-              child: Text("Search product"),
-            ),
-          )
-        : SingleChildScrollView(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ...List.generate(
-                  demoProducts.length,
-                  (index) => GestureDetector(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Provider(
-                          create: (context) => cartList,
-                          builder: (context, child) => DetailsScreen(),
-                        ),
-                        settings: RouteSettings(
-                          arguments: ProductDetailsAgrument(
-                            product: demoProducts[index],
-                            heroTag: demoProducts[index].id + 'search',
+    if (seachInputState.searchInput == "") {
+      return Expanded(
+        child: Center(
+          child: Text("Search for product"),
+        ),
+      );
+    } else {
+      return StreamBuilder<List<Product>>(
+        stream: Stream.fromFuture(
+            AlgoliaSearch().searchProduct(seachInputState.searchInput)),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data!.isNotEmpty) {
+              return SingleChildScrollView(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...List.generate(
+                      snapshot.data!.length,
+                      (index) => GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Provider(
+                              create: (context) => cartList,
+                              builder: (context, child) => DetailsScreen(),
+                            ),
+                            settings: RouteSettings(
+                              arguments: ProductDetailsAgrument(
+                                product: snapshot.data![index],
+                                heroTag: snapshot.data![index].id + 'search',
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    child: Hero(
-                      tag: demoProducts[index].id + 'search',
-                      child: Material(
-                        type: MaterialType.transparency,
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(right: 5),
-                              height: getProportionateScreenWidth(100),
-                              width: getProportionateScreenWidth(100),
-                              child:
-                                  Image.network(demoProducts[index].images[0]),
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: getProportionateScreenWidth(80),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      demoProducts[index].title,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 1,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 16,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    Row(
+                        child: Hero(
+                          tag: snapshot.data![index].id + 'search',
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  height: getProportionateScreenWidth(100),
+                                  width: getProportionateScreenWidth(100),
+                                  child: Image.network(
+                                      snapshot.data![index].images[0]),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    height: getProportionateScreenWidth(80),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       mainAxisAlignment:
                                           MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Row(
-                                          children: [
-                                            Text(
-                                              demoProducts[index]
-                                                  .rating
-                                                  .toString(),
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.w600),
-                                            ),
-                                            const SizedBox(width: 5),
-                                            SvgPicture.asset(
-                                                'assets/icons/Star Icon.svg')
-                                          ],
-                                        ),
                                         Text(
-                                          NumberFormat(',###')
-                                                  .format(
-                                                      demoProducts[index].price)
-                                                  .toString() +
-                                              '₫',
+                                          snapshot.data![index].title,
                                           overflow: TextOverflow.ellipsis,
                                           maxLines: 1,
-                                          style:
-                                              TextStyle(color: cPrimaryColor),
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
                                         ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  snapshot.data![index].rating
+                                                      .toString(),
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.w600),
+                                                ),
+                                                const SizedBox(width: 5),
+                                                SvgPicture.asset(
+                                                    'assets/icons/Star Icon.svg')
+                                              ],
+                                            ),
+                                            Text(
+                                              NumberFormat(',###')
+                                                      .format(snapshot
+                                                          .data![index].price)
+                                                      .toString() +
+                                                  '₫',
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              style: TextStyle(
+                                                  color: cPrimaryColor),
+                                            ),
+                                          ],
+                                        )
                                       ],
-                                    )
-                                  ],
+                                    ),
+                                  ),
                                 ),
-                              ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          );
+                    )
+                  ],
+                ),
+              );
+            } else {
+              Center(child: Text("Found nothing. ¯\_(ツ)_/¯"));
+            }
+          } else {
+            Center(child: Text("Found nothing. ¯\_(ツ)_/¯"));
+          }
+          return LoadingScreen();
+        },
+      );
+    }
   }
 }
